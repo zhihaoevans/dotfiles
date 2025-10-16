dd77beae4bdcac3e94d277fbf76c69987685f99e
zhihaoevans <1659500990@qq.com>
2025-10-16 14:57:22 +0800

    nvim load catppuccin theme

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha", -- 可选: latte, frappe, macchiato, mocha
+      transparent_background = true, -- ✅ 启用透明
+      term_colors = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = { enabled = true },
+      },
+    },
+  },
+}
